0c42faad2cd95757ab4a38a5565f8307e61ce072
Raj Singh <raj02mar2001@gmail.com>
2021-06-23 16:45:01 +0530

    correcting date

--- public/js/date&day.js
+++ public/js/date&day.js
@@ -6,8 +6,8 @@ const getCurrentDay = () => {
     weekday[0] = "Sunday";
     weekday[1] = "Monday";
     weekday[2] = "Tuesday";
-    weekday[3] = "Wedday";
-    weekday[4] = "Thuday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
     weekday[5] = "Friday";
     weekday[6] = "Saturday";
 
